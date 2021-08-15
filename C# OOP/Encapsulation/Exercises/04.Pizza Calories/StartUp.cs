c5789883e9ce5c6aae6b5b5fc5d5be1f7bebc3e8
Nikolay19 <npenchev19@gmail.com>
2021-08-15 22:02:57 +0300

    Encapsulation
    Encapsulation

--- C# OOP/Encapsulation/Exercises/04.Pizza Calories/StartUp.cs
+++ C# OOP/Encapsulation/Exercises/04.Pizza Calories/StartUp.cs
@@ -8,19 +8,26 @@ namespace Pizza_Calories
         static void Main(string[] args)
         {
 
+
             try
             {
                 var pizzaInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
-                var name = pizzaInfo[1];
+                var name = String.Empty;
+                if (pizzaInfo.Length>1)
+                { 
+                    name = pizzaInfo[1];
+
+                }
                 var DoughtType = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
-                var type = DoughtType[1];
-                var tech = DoughtType[2];
-                var cal =int.Parse(DoughtType[3]);
+                var type = Filtered(DoughtType[1]);
+                var tech = Filtered(DoughtType[2]);
+                var cal = int.Parse(DoughtType[3]);
                 var pizza = new Pizza(name, new Dough(type, tech, cal));
                 string line = String.Empty;
                 while ((line = Console.ReadLine()) != "END")
                 {
                     var cmd = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    cmd[1] = Filtered(cmd[1]);
                     var topping = new Topping(cmd[1], int.Parse(cmd[2]));
                     pizza.Add(topping);
                 }
@@ -31,6 +38,20 @@ namespace Pizza_Calories
             {
                 Console.WriteLine(e.Message);
             }
+
+        }
+
+        private static string Filtered(string value)
+        {
+            
+            var array = value.ToCharArray();
+
+            array[0] = char.ToUpper(array[0]);
+            for (int i = 1; i < array.Length; i++)
+            {
+                array[i] = char.ToLower(array[i]);
+            }
+            return new string(array);
 
         }
     }
