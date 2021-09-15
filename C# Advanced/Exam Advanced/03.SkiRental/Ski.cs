56a1c07679a5a088db064237c068d29f26b8b21a
Nikolay19 <npenchev19@gmail.com>
2021-09-15 14:12:00 +0300

    C# Advanced Exam - 26 June 2021
    added 3th problem from the EXAM ( Classes)

---
+++ C# Advanced/Exam Advanced/03.SkiRental/Ski.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace SkiRental
+{
+    public class Ski
+    {
+        public string Manufacturer { get;private set; }
+        public string Model { get; private set; }
+        public int Year { get; private set; }
+
+        public Ski(string manufacturer, string model, int year)
+        {
+            Manufacturer = manufacturer;
+            Model = model;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"{Manufacturer} - {Model} - {Year}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# Advanced/Exam Advanced/03.SkiRental/SkiRental.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+        private readonly List<Ski> data;
+        public string Name { get; private set; }
+        public int Capacity { get;private set; }
+        public int Count => data.Count;
+
+        public SkiRental(string name, int capacity)
+        {
+            this.data = new List<Ski>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public void Add(Ski ski)
+        {
+            if (this.Count < this.Capacity)
+            {
+                this.data.Add(ski);
+            }
+
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+           // var itemToBeRemoved = data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+           return data.Remove(data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model));
+        }
+
+        public Ski GetNewestSki()
+        {
+            var itemToBeReturned = data.OrderByDescending(x => x.Year).FirstOrDefault();
+            return itemToBeReturned;
+
+        }
+
+        public Ski GetSki(string manufacturer, string model)
+        {
+            return data.Find(x => x.Manufacturer == manufacturer && x.Model == model);
+
+        }
+
+        public string GetStatistics()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"The skis stored in {Name}:");
+            for (int i = 0; i < data.Count; i++)
+            {
+                sb.AppendLine($"{data[i]}");
+            }
+            return sb.ToString();
+        }
+    }
+}
