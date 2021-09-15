913cda3c82823b746097d42685709c8c502d2c82
Nikolay19 <npenchev19@gmail.com>
2021-09-15 16:58:40 +0300

    C# Advanced Exam - 28 June 2020
    added 3th problem (Exam/Classes)

---
+++ C# Advanced/Exam Advanced/03.Parking/Parking/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking
+{
+    public class Car
+    {
+        public string Manufacturer { get;private set; }
+        public string Model { get; private set; }
+        public int Year { get; private set; }
+
+        public Car(string manufacturer, string model, int year)
+        {
+            Manufacturer = manufacturer;
+            Model = model;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{Manufacturer} {Model} ({Year})");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Exam Advanced/03.Parking/Parking/Parking.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+        private readonly List<Car> data;
+        public string Type { get; private set; }
+        public int Capacity { get; private set; }
+        public int Count => data.Count;
+
+
+        public Parking(string type, int capacity)
+        {
+            this.data = new List<Car>();
+            Type = type;
+            Capacity = capacity;
+        }
+
+        public void Add(Car car)
+        {
+            if (Capacity > this.data.Count)
+            {
+                data.Add(car);;
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            return data.Remove(data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model));
+        }
+
+        public Car GetLatestCar()
+        {
+            return data.OrderByDescending(x => x.Year).FirstOrDefault();
+        }
+
+        public Car GetCar(string manufacturer, string model)
+        {
+            return data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+        }
+
+        public string GetStatistics()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"The cars are parked in {Type}:");
+            for (int i = 0; i < data.Count; i++)
+            {
+                sb.AppendLine($"{data[i]}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
