86099f695fa7db9c4d9911dc9606b8cf97322fb1
Nikolay19 <npenchev19@gmail.com>
2021-09-15 22:33:54 +0300

    C# Advanced Retake Exam - 14 April 2021
    Cocktail Party

---
+++ C# Advanced/Exam Advanced/03.Cocktail Party/Skeleton/Cocktail.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        private readonly List<Ingredient> Ingredients;
+        public string Name { get; private set; }
+        public int 	Capacity { get; private set; }
+        public int MaxAlcoholLevel { get; private set; }
+        public int CurrentAlcoholLevel => this.Ingredients.Sum(x=>x.Alcohol);
+
+     
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+           
+            Ingredients = new List<Ingredient>();
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+        }
+        
+        public void Add(Ingredient ingredient)
+        {
+           
+            bool check = !Ingredients.Contains(ingredient);
+            bool checkAlco = MaxAlcoholLevel >= ingredient.Alcohol;
+            if (check && checkAlco)
+            {
+                Ingredients.Add(ingredient);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            return Ingredients.Remove(Ingredients.FirstOrDefault(x => x.Name == name));
+
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            return Ingredients.FirstOrDefault(x => x.Name == name);
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return Ingredients.OrderByDescending(x => x.Alcohol).FirstOrDefault();
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+            for (int i = 0; i < Ingredients.Count; i++)
+            {
+                sb.AppendLine($"{Ingredients[i]}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ C# Advanced/Exam Advanced/03.Cocktail Party/Skeleton/Ingredient.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace CocktailParty
+{
+    public class Ingredient
+    {
+        public string Name { get; private set; }
+        public int Alcohol { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Ingredient(string name, int alcohol, int quantity)
+        {
+            Name = name;
+            Alcohol = alcohol;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Ingredient: {Name}");
+            sb.AppendLine($"Quantity: {Quantity}");
+            sb.AppendLine($"Alcohol: {Alcohol}");
+            return sb.ToString().TrimEnd(); 
+        }
+    }
+}
